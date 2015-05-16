3a73d4c4151bf9fd70c194b373787f0859fbeb39
aganoyot <aganoyot@gmail.com>
2015-05-16 21:18:11 +0900

    リニアサーチの実装

---
+++ search/linear_search.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import random
+
+NOT_FOUND = -1
+N = 10
+
+def linearSearch(x, a, num):
+    n = 0
+
+    # 配列の範囲内で目的の値を探す
+    while n < num and a[n] != x:
+        n = n + 1
+
+    if n < num:
+        return n
+    
+    return NOT_FOUND
+
+if __name__ == '__main__':
+    array = []
+    # 配列を作成する
+    print 'array '
+    for i in range(N):
+        number = random.randint(0, 20) 
+        array.append(number)
+        print '[%d]:%d ' % (i, number)
+
+    # print array
+
+    # 探す配列を入力 TODO: 数値以外を入力した場合の例外チェック
+    print '\n何を探しますか:'
+    i = int(raw_input())
+
+    # リニアサーチ
+    r = linearSearch(i, array, N)
+
+    if r == NOT_FOUND:
+        print '%dは見つかりません\n' % (i)
+    else:
+        print '%dは%d番目です\n' % (i,r)
